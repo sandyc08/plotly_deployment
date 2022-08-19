13608150fea13cf3d20b108b61c524d0ed052fd9
Sandy Cisneros <sandyc08@yahoo.com>
2022-08-19 14:47:02 -0700

    bubble chart and customization

--- charts.js
+++ charts.js
@@ -98,29 +98,29 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
-  });
-
+    
     // 1. Create the trace for the bubble chart
     var bubbleData = [{
-      xaxis: otu_ids.slice(0,10).reverse(),
-      yaxis: sample_values.slice(0,10).reverse(),
-      text: otu_labels.slice(0,10).reverse(),
+      y: otu_ids,
+      x: sample_values,
+      text: otu_labels,
       mode: 'markers',
       marker: {
-        size: sample_values.slice(0,10).reverse(),
-        color: otu_ids.slice(0,10).reverse(),
+        size: sample_values,
+        color: otu_ids,
         colorscale: 'Earth'
       }
     }
   ];
-    // 2. Create the layout for the bubble chart.
-    var bubbleLayout = {
-      title: "Bacteria Cultures Per Sample",
-      xaxis: {title: "Sample Values"},
-      yaxis: {title: "ID's"}
-      
-    };
-
-    // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
-  }
+  // 2. Create the layout for the bubble chart.
+  var bubbleLayout = {
+    title: "Bacteria Cultures Per Sample",
+    xaxis: {title: "Sample Values"},
+    yaxis: {title: "ID's"}
+    
+  };
+  
+  // 3. Use Plotly to plot the data with the layout.
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+});
+}
